7191f333724cdf6c3e538f244b6ff2845802573f
drewatienza <atienza.drew@gmail.com>
2019-06-26 09:37:20 -0700

    created profile form

--- client/src/components/common/SelectListGroup.js
+++ client/src/components/common/SelectListGroup.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 
 const SelectListGroup = ({ name, value, error, info, onChange, options }) => {
   const selectOptions = options.map(option => (
-    <opton key={option.label} value={option.value}>
+    <option key={option.label} value={option.value}>
       {option.label}
-    </opton>
+    </option>
   ));
   return (
     <div className="form-group">
